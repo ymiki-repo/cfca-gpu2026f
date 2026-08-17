31777e5b0b8418b3f8cc72d1b03fd12ca7adfa39
Yohei MIKI <ymiki@cc.u-tokyo.ac.jp>
2026-08-17 15:18:54 +0900

    update job script for OpenMP

--- sh/cfca/run_cpu.sh
+++ sh/cfca/run_cpu.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 #SBATCH --partition=gpuws
-#SBATCH --gres=gpu:1
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --cpus-per-task=8
 #SBATCH --time=00:05:00
 
 module purge
 module load nvhpc/25.7
 
 cd ${SLURM_SUBMIT_DIR}
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export OMP_PROC_BIND=close
+export OMP_PLACES=cores
 
 ./vecadd
